03c6a5acbd0424de54d5bd148c0e9ae5e9212f77
ugartepenamanuel <mpugartepena@gmail.com>
2020-10-16 01:20:57 +0200

    PracticaCoches

---
+++ src/Coche.js
@@ -0,0 +1,51 @@
+db.coche.insertMany([
+
+    {
+        _id: 1,
+        Marca: "Ferrari",
+        Modelo: "GTC4",
+        Año: "2016",
+        Precio: "265708"
+    },
+    
+    {
+        _id: 2,
+        Marca: "Renault",
+        Modelo: "Clio",
+        Año: "1990",
+        Precio: "7000"
+    },
+    
+    {
+        _id: 3,
+        Marca: "Citroen",
+        Modelo: "Cactus",
+        Año: "2016",
+        Precio: "10000"
+    },
+    
+    {
+        _id: 4,
+        Marca: "Lamborghini",
+        Modelo: "Huracan",
+        Año: "2013",
+        Precio: "300000"
+    },
+    
+    {
+        _id: 5,
+        Marca: "Wolsvagen",
+        Modelo: "Golf",
+        Año: "2000",
+        Precio: "12000"
+    },
+    
+    {
+        _id: 6,
+        Marca: "Wolsvagen",
+        Modelo: "Polo",
+        Año: "2002",
+        Precio: "10000"
+    },
+
+    ])
